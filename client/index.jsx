5976616faef5606e6f83ea5d6ef7f1e72bfc5186
simenhaga <simen.haga@lyse.net>
2022-05-04 10:55:14 +0200

    Added grid layout. (Holy Grail Layout).

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,33 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function Frontpage() {
+  return (
+    <div className="container">
+      <header>
+        <div>Hello header</div>
+      </header>
+
+      <nav>
+        <div>Hello nav</div>
+      </nav>
+
+      <main>
+        <div>Hello main</div>
+      </main>
+    </div>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Frontpage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
