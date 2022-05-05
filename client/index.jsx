2221401932756ea85ce866d6229cc84ce96af00a
simenhaga <simen.haga@lyse.net>
2022-05-05 19:14:51 +0200

    Extracted useLoading.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 import { fetchJSON } from "./utils/fetchJSON";
 import { ArticleCard } from "./components/articleCard";
+import { useLoading } from "./utils/useLoading";
 
 const ProfileContext = React.createContext({
   userinfo: undefined,
@@ -187,28 +188,6 @@ function ListTitles() {
       ))}
     </div>
   );
-}
-
-function useLoading(loadingFunction) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
-  const [data, setData] = useState();
-
-  async function load() {
-    try {
-      setLoading(true);
-      setData(await loadingFunction());
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-  useEffect(() => {
-    load();
-  }, []);
-
-  return { loading, error, data };
 }
 
 function ListTopics() {
